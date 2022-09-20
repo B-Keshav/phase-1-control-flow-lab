241a747ef5f527eaad34f9dbdf3a75adc805ddf6
Keshav Bhati <keshavbhati2000@gmail.com>
2022-09-19 22:06:59 -0400

    commit tree 5282173f3312f9e72f4a5c07d4569ba41eab20d7

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <= 400){
+    return "This one is on me!";
+  }
+  else if(distance < 2000){
+    return "That will be twenty bucks."
+  }
+  else if(distance < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  let response;
+  switch(amount){
+    case 'generous':
+      response = "Thank you so much."
+      break;
+    case 'not as generous':
+      response = "Thank you."
+      break;
+    default:
+      response = "Bye."
+  }
+  return response;
 }
